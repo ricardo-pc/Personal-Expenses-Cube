9bb8d02794f665567c119b887bbd9efe0457c794
Ricardo Pérez Castillo <59816981+ricardo-pc@users.noreply.github.com>
2025-01-04 19:15:20 -0600

    Added files stored locally
    Removed personal information and added demo data

---
+++ 01 Scripts/config.py
@@ -0,0 +1,5 @@
+# config.py
+
+current_month = "01"
+current_month_text = "January"
+current_year = 2024
---
+++ 01 Scripts/transaction_subtype_mapping.py
@@ -0,0 +1,29 @@
+transaction_subtype_mapping = {
+    "COMPENSACION POR RETRASO": "Compensation Credit",
+    "RETIRO CAJERO AUTOMATICO": "Cash Withdrawal",
+    "GRACIAS POR SU PAGO CON CARGO A BBVA": "Credit Card Payment",
+    "AMERICAN EXPRESS    01234": "Credit Card Payment",
+    "PAGO TARJETA DE CREDITO": "Credit Card Payment",
+    "(BE) Pago servicio: TARJETA DE CREDITO B": "Credit Card Payment",
+    "Pago servicio: TARJETA DE CREDITO":"Credit Card Payment",
+    "SPEI ENVIADO NAFIN": "Investment",
+    "SPEI ENVIADO GBM": "Investment",
+    "Apertura de INVERSIÓN HEY":"Investment",
+    "YOUR NAME. HeyAhorra":"Investment",
+    "Recepcion de cuenta":"Investment",
+    "Gastos Medicos": "Medical Reimbursement",
+    "Nomina": "Payroll",
+    "Pagodenomina": "Payroll",
+    "Nomina": "Payroll",
+    "SPEI RECIBIDO": "3rd Party Transfer",
+    "PAGO CUENTA DE TERCERO": "3rd Party Transfer",
+    "SPEI RECIBIDOBANORTE": "3rd Party Transfer",
+    "SPEI DEVUELTOSTP": "Canceled Transfer",
+    "SPEI DEVUELTONAFIN": "Canceled Transfer",
+    "Reembolso Viaticos": "Work Expenses Reimbursement",
+    "MONTO A DIFERIR MESES EN AUTOMATICO": "Deferred Payment",
+    "MESES EN AUTOMATICO NACIONAL": "Monthly Payment",
+    "SPEI.BBVA MEXICO.012345.YOUR NAME":"Intra account transfers"
+
+
+}
